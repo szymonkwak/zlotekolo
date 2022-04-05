040eea4c5b92eb35f6d091df39c82810bddf20e5
Marcin Woś <marcin.wos@coderscrew.pl>
2022-04-05 20:51:38 +0200

    removed database

--- client/src/tests/utils.tsx
+++ client/src/tests/utils.tsx
@@ -3,11 +3,11 @@ import { render, RenderOptions } from '@testing-library/react';
 import { FC, ReactElement } from 'react';
 import { QueryClientProvider } from '~/api/QueryClientProvider';
 
-const AllTheProviders: FC = ({ children }) => {
+const WithProviders: FC = ({ children }) => {
   return <QueryClientProvider>{children}</QueryClientProvider>;
 };
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) => render(ui, { wrapper: WithProviders, ...options });
 
 export * from '@testing-library/react';
 export { customRender as render };
