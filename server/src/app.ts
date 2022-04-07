e5451df75a26a3fe210ddb8ace4e0758be57514d
Mateusz <matzaj101@gmail.com>
2022-04-07 09:44:41 +0200

    change

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,10 +10,11 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(cookieParser());
-app.use('/', (req, res) => {
-  res.send('ok');
-});
 app.use('/api', express.json());
 app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+  res.send('ok');
+});
 
 export { app };
