bdeafb9cd9c8f2b0c7b93632a9a2aaa468a21f0c
Szymon Kwak <szymon.kwak@gmail.com>
2022-04-13 21:52:20 +0200

    change userin  prisma seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -5,5 +5,7 @@ const prisma = new PrismaClient();
 (async () => {
   await prisma.user.deleteMany();
 
-  await prisma.user.create({ data: { id: '1', username: 'Krzysiu', nickname: 'Krzychu007', avatar: 'emptyhttps', toWorkDistance: 2.5 } });
+  await prisma.user.create({
+    data: { id: '1', username: 'Krzysiu', nickname: 'Krzychu007', email: 'krzy@szt.of', avatar: 'emptyhttps', toWorkDistance: 2.5 },
+  });
 })();
