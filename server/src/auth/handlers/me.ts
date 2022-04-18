665016710240bffe63ca0ac43c60aabc6b9d237f
Szymon Kwak <40847193+szymonkwak@users.noreply.github.com>
2022-04-18 23:02:52 +0200

    7 authentication middleware (#20)
    * authMiddleware added
    
    * authMiddleware added to me endpoint & added tests
    
    * changes after review

--- server/src/auth/handlers/me.ts
+++ server/src/auth/handlers/me.ts
@@ -1,8 +1,5 @@
 import { RequestHandler } from 'express';
 
-import { prisma } from '~/common/prisma';
-
-export const getMe: RequestHandler = async (req, res) => {
-  const user = await prisma.user.findFirst();
-  res.send(user);
+export const getMe: RequestHandler = (req, res) => {
+  res.send(req.user);
 };
