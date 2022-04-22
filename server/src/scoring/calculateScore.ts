fc03a87734a196e73628ecb877f854201d4aa303
Ewelina W <ewelina.wystepek@gmail.com>
2022-04-22 12:25:26 +0200

    work in progress

--- server/src/scoring/calculateScore.ts
+++ server/src/scoring/calculateScore.ts
@@ -1,11 +1,41 @@
-import { ContractType,User } from "@prisma/client";
+import { ContractType, User, Point } from "@prisma/client";
 
 
-export const calculateScore = (user: User): number => {
+const calculateBusinessDays = () => {
+  const day = new Date();
+  const month = day.getMonth();
+  day.setDate(1);
+  let businessDays = 0;
+
+  while (day.getMonth() === month) {
+    if (day.getDay() >= 1 && day.getDay() < 6) {
+      businessDays += 1;
+    }
+
+    day.setDate(day.getDate() + 1);
+  }
+  return businessDays;
+}
+
+export const calculateScore = (user: User & { points: Point[] }): number => {
+
+  const today = new Date();
+
+
+
   if (user.contractType === ContractType.FULL_TIME) {
-    const score = 0;
-    return score;
-  } else {
-    return 1;
+    const filterPoints = user.points.filter((point) => {
+
+      const date = point.scoredAt;
+
+      if (date.getFullYear() === today.getFullYear() && date.getMonth() === today.getMonth() && (date.getDay() >= 1 && date.getDay() < 6)) {
+
+      }
+    })
+  })
+
+  return score;
+} else {
+  return 1;
   }
 };
