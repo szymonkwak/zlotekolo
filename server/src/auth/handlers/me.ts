7e61704d1b6056b43b0afe44ce2e54966cfb59cf
Ewelina W <ewelina.wystepek@gmail.com>
2022-04-23 19:37:29 +0200

    enhanced me endpoint

--- server/src/auth/handlers/me.ts
+++ server/src/auth/handlers/me.ts
@@ -1,5 +1,22 @@
 import { RequestHandler } from 'express';
+import { Trip, User } from '@prisma/client';
+import { calculateScore, calculateBusinessDays, countTrips } from '~/scoring/calculateScore';
+
+const getTotalDistance = (user: User & { trips: Trip[] }): number => {
+  return user.toWorkDistance * user.trips.length;
+}
 
 export const getMe: RequestHandler = (req, res) => {
-  res.send(req.user);
+  const user: User & { trips: Trip[] } = req.user as any;
+
+  const score = calculateScore(user);
+
+  res.send({
+    ...user,
+    score,
+    trips: countTrips(user.trips),
+    maxTrips: calculateBusinessDays() * 2,
+    totalDistance: getTotalDistance(user),
+    totalMonthDistance: score * user.toWorkDistance,
+  });
 };
