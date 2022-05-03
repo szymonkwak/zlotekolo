96c0d36f5ada812ee8c44510d45dcad485b4bce5
Szymon Kwak <szymon.kwak@gmail.com>
2022-05-03 21:52:13 +0200

    shit

--- client/src/pages/Main/subcomponents/CalendarWithTrips.tsx
+++ client/src/pages/Main/subcomponents/CalendarWithTrips.tsx
@@ -1,11 +1,24 @@
 import { Accordion, Center, Indicator, Paper, Title } from '@mantine/core';
 import { Calendar } from '@mantine/dates';
+import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
-function CalendarWithTrips() {
-  const find = (a: number[], nr: number) => {
-    return a.find((el) => el === nr);
-  };
-  const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { QueryKeys } from '~/api/QueryKeys';
+import { LoaderCentered } from '~/components/Loader/LoaderCentered';
+
+import { getUserTrips } from '../requests/getUserTrips';
+import { tripsAtDay } from './calendarWithTripsCalculations';
+
+const CalendarWithTrips = () => {
+  const [tripsWithCount, setTripsWithCount] = useState<ReturnType<typeof tripsAtDay>>([{ tripDate: '2022-05-01', noOfTrips: 8 }]);
+  const { data: trips, isLoading, isError, isSuccess } = useQuery(QueryKeys.Trips, getUserTrips);
+
+  useEffect(() => {
+    setTripsWithCount(tripsAtDay(trips));
+    console.log(tripsWithCount);
+  }, [trips]);
+
+  if (isLoading) return <LoaderCentered />;
 
   return (
     <Paper component="form" withBorder={true} p="md" m="sm">
@@ -18,14 +31,54 @@ function CalendarWithTrips() {
               size="md"
               allowLevelChange={false}
               renderDay={(date) => {
-                const day = date.getDate();
-                return find(a, day) ? (
-                  <Indicator label={find(a, day)} size={15} offset={7}>
-                    <div>{day}</div>
+                // console.log(date.getDate());
+                // console.log(date.toISOString().slice(0, 10));
+                // return <div>{date.getDate()}</div>;
+
+                const arr = [2, 5];
+                if (arr.includes(date.getDay())) {
+                  //   console.log('oo');
+                }
+                // console.log(tripsWithCount);
+                // if (tripsWithCount?.includes({ tripDate: date.toISOString().slice(0, 10) })) {
+                //   console.log('it does');
+                // }
+
+                if (tripsWithCount?.find((trip) => trip.tripDate === date.toISOString().slice(0, 10))) {
+                  const label = tripsWithCount.find((trip) => trip.tripDate === date.toISOString().slice(0, 10));
+                  return (
+                    <Indicator label={label?.noOfTrips} size={15} offset={7}>
+                      <div>{date.getDate()}</div>
+                    </Indicator>
+                  );
+                } else {
+                  return <div>{date.getDate()}</div>;
+                }
+
+                return arr.includes(date.getDay()) ? (
+                  <Indicator label={date.getDay()} size={15} offset={7}>
+                    <div>{date.getDate()}</div>
                   </Indicator>
                 ) : (
-                  <div>{day}</div>
+                  <div>{date.getDate()}</div>
                 );
+
+                // if tripsWithCount?.indexOf({tripDate: date.getDate()}).toString().includes(date.toISOString().slice(0, 10)))
+
+                return tripsWithCount?.map(
+                  (trip) =>
+                    trip.tripDate === date.toISOString().slice(0, 10) ? (
+                      <>
+                        <Indicator label={trip.noOfTrips} size={15} offset={7}>
+                          <div>{date.getDate() - 1}</div>
+                        </Indicator>
+                      </>
+                    ) : null
+                  //   <div>1</div>
+                  // <div>{date.getDate()}</div>
+                );
+                console.log('not');
+                return <div>{date.getDate()}</div>;
               }}
             />
           </Center>
@@ -33,6 +86,6 @@ function CalendarWithTrips() {
       </Accordion>
     </Paper>
   );
-}
+};
 
 export default CalendarWithTrips;
