99509150cc04a96938c5dca2656856971dafe3db
Szymon Kwak <szymon.kwak@gmail.com>
2022-05-08 17:30:48 +0200

    refactor countTrips function

--- server/src/scoring/calculateScore.ts
+++ server/src/scoring/calculateScore.ts
@@ -3,19 +3,17 @@ import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjsBusinessTime from 'dayjs-business-time';
 
+import { getDateJs, getNowYYYY_MM } from './services/dates';
 import { holidays } from './services/holidays';
+import { parseDateString } from './services/trip';
 
 dayjs.extend(customParseFormat);
 dayjs.extend(dayjsBusinessTime);
 dayjs.setHolidays(holidays);
 
-// const isBusinesDay = (day: Date) => day.getDay() >= 1 && day.getDay() < 6;
-const isSameYear = (a: Date, b: Date) => a.getFullYear() === b.getFullYear();
-const isSameMonth = (a: Date, b: Date) => a.getMonth() === b.getMonth();
-
-export const calculateBusinessDays = (YYYY_MM = `${dayjs().year()}_${dayjs().month() + 1}`) => {
-  if (!dayjs(YYYY_MM, ['YYYY_MM', 'YYYY_M']).isValid()) return -1;
-  const date = dayjs(YYYY_MM, ['YYYY_MM', 'YYYY_M']).set('date', 1);
+export const calculateBusinessDays = (YYYY_MM = getNowYYYY_MM()) => {
+  const date = getDateJs(YYYY_MM);
+  if (!date) return -1;
 
   let businessDays = 0;
   for (let i = 1; i <= date.daysInMonth(); i++) {
@@ -26,20 +24,16 @@ export const calculateBusinessDays = (YYYY_MM = `${dayjs().year()}_${dayjs().mon
   return businessDays;
 };
 
-export const countTrips = (trips: Trip[]): number => {
-  const today = new Date();
-  const eligibleTrips = trips.filter((trip) => {
-    const tripDate = trip.dayOfTrip;
-
-    return isSameYear(tripDate, today) && isSameMonth(tripDate, today);
-    //  && isBusinesDay(tripDate);
-  });
+export const countTrips = (trips: Trip[], YYYY_MM = getNowYYYY_MM()) => {
+  const date = getDateJs(YYYY_MM);
+  const eligibleTrips = trips.filter((trip) => parseDateString(trip.dayOfTrip.toISOString()).isSame(date, 'month'));
   return eligibleTrips.length;
 };
 
-export const calculateScore = (user: User & { trips: Trip[] }): number => {
+export const calculateScore = (user: User & { trips: Trip[] }) => {
   const tripsCount = countTrips(user.trips);
 
+  // TODO Ta część jest do zmiany - możliwość delegacji
   if (user.contractType === ContractType.FULL_TIME) {
     const totalMonthlyScore = calculateBusinessDays() * 2;
     return tripsCount / totalMonthlyScore;
