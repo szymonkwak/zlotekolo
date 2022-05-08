607277fd6ca6f9aa4a0f834fec4349930079efdc
Szymon Kwak <szymon.kwak@gmail.com>
2022-05-08 16:47:10 +0200

    corrected calculateBusinessDays function

--- server/src/scoring/calculateScore.ts
+++ server/src/scoring/calculateScore.ts
@@ -1,20 +1,27 @@
 import { ContractType, Trip, User } from '@prisma/client';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dayjsBusinessTime from 'dayjs-business-time';
 
-const isBusinesDay = (day: Date) => day.getDay() >= 1 && day.getDay() < 6;
+import { holidays } from './services/holidays';
+
+dayjs.extend(customParseFormat);
+dayjs.extend(dayjsBusinessTime);
+dayjs.setHolidays(holidays);
+
+// const isBusinesDay = (day: Date) => day.getDay() >= 1 && day.getDay() < 6;
 const isSameYear = (a: Date, b: Date) => a.getFullYear() === b.getFullYear();
 const isSameMonth = (a: Date, b: Date) => a.getMonth() === b.getMonth();
 
-export const calculateBusinessDays = () => {
-  const day = new Date();
-  const month = day.getMonth();
-  day.setDate(1);
-  let businessDays = 0;
+export const calculateBusinessDays = (YYYY_MM = `${dayjs().year()}_${dayjs().month() + 1}`) => {
+  if (!dayjs(YYYY_MM, ['YYYY_MM', 'YYYY_M']).isValid()) return -1;
+  const date = dayjs(YYYY_MM, ['YYYY_MM', 'YYYY_M']).set('date', 1);
 
-  while (day.getMonth() === month) {
-    if (isBusinesDay(day)) {
-      businessDays += 1;
+  let businessDays = 0;
+  for (let i = 1; i <= date.daysInMonth(); i++) {
+    if (date.set('date', i).isBusinessDay()) {
+      businessDays++;
     }
-    day.setDate(day.getDate() + 1);
   }
   return businessDays;
 };
@@ -24,7 +31,8 @@ export const countTrips = (trips: Trip[]): number => {
   const eligibleTrips = trips.filter((trip) => {
     const tripDate = trip.dayOfTrip;
 
-    return isSameYear(tripDate, today) && isSameMonth(tripDate, today) && isBusinesDay(tripDate);
+    return isSameYear(tripDate, today) && isSameMonth(tripDate, today);
+    //  && isBusinesDay(tripDate);
   });
   return eligibleTrips.length;
 };
