cddfe5020076524fa063b94019176c0dc5414ccd
Szymon Kwak <szymon.kwak@gmail.com>
2022-05-13 21:15:01 +0200

    change schema to array with businessTrips

--- server/src/scoring/handlers/businessTrips.ts
+++ server/src/scoring/handlers/businessTrips.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import dayjsBusinessTime from 'dayjs-business-time';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
@@ -8,6 +9,7 @@ import { prisma } from '~/common/prisma';
 import { holidays } from '../services/holidays';
 import { parseDateString } from '../services/trip';
 
+dayjs.extend(isSameOrBefore);
 dayjs.extend(dayjsBusinessTime);
 dayjs.setHolidays(holidays);
 
@@ -15,21 +17,23 @@ export const addBusinessTrip: RequestHandler = async (req, res) => {
   console.log(req.body);
   const { from, to } = req.body;
 
-  let duration = 1;
+  let duration = 0;
+  const businessTripDates: Date[] = [];
 
-  if (to) {
-    let loopDate = parseDateString(from);
-    while (loopDate.isBefore(parseDateString(to))) {
-      if (loopDate.isBusinessDay()) duration++;
-      loopDate = loopDate.add(1, 'day');
+  let loopDate = parseDateString(from);
+  while (loopDate.isSameOrBefore(parseDateString(to))) {
+    if (loopDate.isBusinessDay()) {
+      duration++;
+      businessTripDates.push(loopDate.toDate());
     }
+    loopDate = loopDate.add(1, 'day');
   }
   console.log(duration);
+  console.log(businessTripDates);
 
   const businessTrip = await prisma.bussinesTrip.create({
     data: {
-      from: parseDateString(from).toDate(),
-      to: to ? parseDateString(to).toDate() : parseDateString(from).toDate(),
+      dates: businessTripDates,
       duration,
     },
   });
