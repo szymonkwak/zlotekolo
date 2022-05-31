feaeac68dd16fe660b3181260117f898977f55bb
Szymon Kwak <szymon.kwak@gmail.com>
2022-05-31 23:44:11 +0200

    configure dotenv

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,11 @@
+import 'dotenv/config';
+
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
 
 import { authRouter } from './auth/router';
 import { scoringRouter } from './scoring/routes';
-
-dotenv.config();
 
 if (!process.env.CLIENT_URL) {
   // eslint-disable-next-line no-console
